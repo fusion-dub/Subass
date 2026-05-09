4bdc42566e68bd88a662531352707ebc0a702042
fusion-dub <fusion.dub.studio@gmail.com>
2026-05-09 16:19:48 +0300

    Create get_user_profile_details.sql

---
+++ plugin/supabase/get_user_profile_details.sql
@@ -0,0 +1,24 @@
+-- SQL команда для отримання деталей профілю
+CREATE OR REPLACE FUNCTION get_user_profile_details(target_id TEXT)
+RETURNS JSONB AS $$
+DECLARE
+    result JSONB;
+BEGIN
+    SELECT jsonb_build_object(
+        'dubber_name', dubber_name,
+        'dubber_bio', dubber_bio,
+        'dubber_contact', dubber_contact,
+        'dubber_samples', dubber_samples,
+        'dubber_equipment', dubber_equipment,
+        'dubber_conditions', dubber_conditions,
+        'dubber_voice', dubber_voice,
+        'dubber_timbre', dubber_timbre,
+        'last_active', last_active,
+        'machine_id', machine_id
+    ) INTO result
+    FROM users
+    WHERE machine_id = target_id;
+    
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
