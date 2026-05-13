c6f3d83930369b2449115da39ce6b792a73ac92e
fusion-dub <fusion.dub.studio@gmail.com>
2026-05-13 09:58:05 +0300

    Update get_all_talents.sql

--- plugin/supabase/get_all_talents.sql
+++ plugin/supabase/get_all_talents.sql
@@ -23,7 +23,7 @@ BEGIN
         v_looking_for_talents_count
     FROM users
     WHERE 
-        (dubber_status IS NULL OR dubber_status != 'Приховати аккаунт')
+        (dubber_status IS NULL OR dubber_status != 'Приховати аккаунт') AND (dubber_force_hide IS NOT TRUE)
         AND (p_filters->>'voice' IS NULL OR dubber_voice = p_filters->>'voice')
         AND (p_filters->>'conditions' IS NULL OR dubber_conditions = p_filters->>'conditions')
         AND (p_filters->>'vocals' IS NULL OR dubber_vocals = p_filters->>'vocals')
@@ -63,7 +63,7 @@ BEGIN
             last_active
         FROM users
         WHERE 
-            (dubber_status IS NULL OR dubber_status != 'Приховати аккаунт')
+            (dubber_status IS NULL OR dubber_status != 'Приховати аккаунт') AND (dubber_force_hide IS NOT TRUE)
             AND (p_filters->>'voice' IS NULL OR dubber_voice = p_filters->>'voice')
             AND (p_filters->>'conditions' IS NULL OR dubber_conditions = p_filters->>'conditions')
             AND (p_filters->>'vocals' IS NULL OR dubber_vocals = p_filters->>'vocals')
